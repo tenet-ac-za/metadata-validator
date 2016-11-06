d22bea9e18091d641ab80b28eb0313cfaca4504e
Guy Halse <guy@tenet.ac.za>
2016-11-06 09:37:26 +0200

    Fix HTML so it validates with https://validator.w3.org/

--- ui/header.inc.php
+++ ui/header.inc.php
@@ -20,10 +20,9 @@ if (preg_match('/\.local$/', $_SERVER['SERVER_NAME'])) {
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
 	<meta name="robots" content="index, nofollow">
-	<link rel="license" content="https://opensource.org/licenses/MIT">
+	<link rel="license" href="https://github.com/safire-ac-za/metadata-validator/blob/master/LICENSE">
 
 	<!-- these are SAFIRE-specific -->
 	<title>validator.<?php echo $domain ?></title>
@@ -32,7 +31,6 @@ if (preg_match('/\.local$/', $_SERVER['SERVER_NAME'])) {
 	<link rel="icon" type="image/png" sizes="16x16" href="https://discovery.<?php echo $domain ?>/module.php/safire/resources/favicon-16x16.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="https://discovery.<?php echo $domain ?>/module.php/safire/resources/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="72x72" href="https://discovery.<?php echo $domain ?>/module.php/safire/resources/favicon-72x72.png">
-	<script type="text/javascript" src="https://discovery.<?php echo $domain ?>/resources/script.js"></script>
 	<meta name="theme-color" content="#5da9dd">
 
 	<!-- these are the bits you need to keep in a new skin -->
@@ -45,6 +43,16 @@ if (preg_match('/\.local$/', $_SERVER['SERVER_NAME'])) {
             integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/ace/1.2.5/ace.js"></script>
 	<script src="validate.js"></script>
+
+    <!-- this is for the github-corners - https://github.com/tholman/github-corners -->
+    <style>
+        .github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}
+        @keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}
+        @media (max-width:500px){
+            .github-corner:hover .octo-arm{animation:none}
+            .github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}
+        }
+    </style>
 </head>
 <body>
 
@@ -56,14 +64,6 @@ if (preg_match('/\.local$/', $_SERVER['SERVER_NAME'])) {
 		<path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path>
 	</svg>
 </a>
-<style>
-	.github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}
-	@keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}
-	@media (max-width:500px){
-		.github-corner:hover .octo-arm{animation:none}
-		.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}
-	}
-</style>
 <!-- end of github-corners -->
 
 <div id="wrap">
@@ -72,4 +72,4 @@ if (preg_match('/\.local$/', $_SERVER['SERVER_NAME'])) {
         <a title="South African Identity Federation" href="https://safire.ac.za/"><img src="https://discovery.<?php echo $domain ?>/module.php/safire/resources/SAFIRE_P_White_SimpleSAML.svg" alt="SAFIRE" style="float: right; display: inline; height: 72px;"></a>
 		<h1><a style="text-decoration: none; color: white" href="/">validator.<?php echo $domain ?></a></h1>
 	</div>
-	<div id="languagebar">English</div>	<div id="content">
+	<div id="languagebar">&nbsp;</div>	<div id="content">
